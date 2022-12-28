6a1158d66a9dd5208150b41aa024217fdc23ba0c
FilosofiaJulia <filosofiajulia@gmail.com>
2022-12-28 17:32:43 +0300

    Сжала css и js файлы

--- js/toys.js
+++ js/toys.js
@@ -1,15 +1,1 @@
-const rangeInput = document.querySelector('.range-field');
-
-function handleInputChange(event) {
-    let target = event.target;
-    if (event.target.type !== 'range') {
-      target = document.getElementById('range');
-    } 
-    const minValue = target.min;
-    const maxValue = target.max;
-    const currentValue = target.value;
-    
-    target.style.backgroundSize = (currentValue - minValue) * 100 / (maxValue - minValue) + '% 100%';
-  }
-  
-    rangeInput.addEventListener('input', handleInputChange);
+const rangeInput=document.querySelector(".range-field");function handleInputChange(e){let n=e.target;"range"!==e.target.type&&(n=document.getElementById("range"));var t=n.min,a=n.max,e=n.value;n.style.backgroundSize=100*(e-t)/(a-t)+"% 100%"}rangeInput.addEventListener("input",handleInputChange);
